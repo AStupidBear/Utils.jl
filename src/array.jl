5cb6fd3cab1b9e48460b302b4096f2f49018ee70
Yao Lu <luyaocns@gmail.com>
2018-03-29 15:25:37 +0800

    rcount(x::Tuple), cview(a, i), rview(a, i)

--- src/array.jl
+++ src/array.jl
@@ -38,7 +38,7 @@ csize(a) = (ndims(a) == 1 ? size(a) : size(a)[1:end-1])
 csize(a, n) = tuple(csize(a)..., n) # size if you had n columns
 clength(a) = (ndims(a) == 1 ? 1 : stride(a, ndims(a)))
 ccount(a) = (ndims(a) == 1 ? length(a) : size(a, ndims(a)))
-cview(a, i) = (ndims(a) == 1 ? a[i] : view(a, ntuple(i->(:), ndims(a) - 1)..., i))
+cview(a, i) = (ndims(a) == 1 ? view(a, i) : view(a, ntuple(i->(:), ndims(a) - 1)..., i))
 cget(a, i) = (ndims(a) == 1 ? a[i] : getindex(a, ntuple(i->(:), ndims(a)-1)..., i))
 cset!(a, x, i) = (ndims(a) == 1 ? (a[i] = x) : setindex!(a, x, ntuple(i->(:), ndims(a) - 1)..., i))
 size2(y) = (nd = ndims(y); (nd == 1 ? (length(y), 1) : (stride(y, nd), size(y, nd)))) # size as a matrix
@@ -49,7 +49,7 @@ rsize(a) = (ndims(a)==1 ? size(a) : size(a)[2:end])
 rsize(a, n) = tuple(n, rsize(a)...) # size if you had n columns
 rlength(a) = (ndims(a) == 1 ? length(a) : stride(a, ndims(a)))
 rcount(a) = (ndims(a) == 1 ? length(a) : size(a, 1))
-rview(a, i) = (ndims(a) == 1 ? a[i] : view(a, i, ntuple(i->(:), ndims(a) - 1)...))
+rview(a, i) = (ndims(a) == 1 ? view(a, i) : view(a, i, ntuple(i->(:), ndims(a) - 1)...))
 rget(a, i) = (ndims(a) == 1 ? a[i] : getindex(a, i, ntuple(i->(:), ndims(a)-1)...))
 rset!(a, x, i) = (ndims(a)==1 ? (a[i] = x) : setindex!(a, x, i, ntuple(i->(:), ndims(a)-1)...))
 size1(y) = (nd = ndims(y); (nd == 1 ? (length(y), 1) : (size(y, 1), prod(size(y)[2:end])))) # size as a matrix
@@ -66,6 +66,7 @@ Base.transpose(x::Tuple) = transpose.(x)
 Base.ctranspose(x::Tuple) = ctranspose.(x)
 
 ccount(x::Tuple) = ccount(x[1])
+rcount(x::Tuple) = rcount(x[1])
 
 # function sp_A_mul_B!(y, rowptr, colptr, I, J, A, x)
 #     fill!(y, zero(eltype(y)))
