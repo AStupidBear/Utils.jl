8f9d24282318fcdc8bf49df502624fa87ce02f26
Yao Lu <luyaocns@gmail.com>
2018-05-10 17:56:57 +0800

    throttle

--- src/function.jl
+++ src/function.jl
@@ -5,3 +5,36 @@ end
 
 export partial
 partial(f, a...; b...) = ((x...; y...) -> f(a..., x...; b..., y...))
+
+export throttle
+function throttle(f, timeout; leading = true, trailing = false)
+  cooldown = true
+  later = nothing
+  result = nothing
+
+  function throttled(args...; kwargs...)
+    yield()
+
+    if cooldown
+      if leading
+        result = f(args...; kwargs...)
+      else
+        later = () -> f(args...; kwargs...)
+      end
+
+      cooldown = false
+      @schedule try
+        while (sleep(timeout); later != nothing)
+          later()
+          later = nothing
+        end
+      finally
+        cooldown = true
+      end
+    elseif trailing
+      later = () -> (result = f(args...; kwargs...))
+    end
+
+    return result
+  end
+end
