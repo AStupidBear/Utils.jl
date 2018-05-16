141fc2044c4ba2d7ca97517c14baaaad5445b25f
Yao Lu <luyaocns@gmail.com>
2018-05-16 21:02:49 +0800

    @npwrite

--- src/io.jl
+++ src/io.jl
@@ -3,3 +3,8 @@ readabsdir(dir) = map(file -> joinpath(dir, file), readdir(dir))
 
 export readall
 readall(f::Union{IO, AbstractString}, T) = reinterpret(T, read(f))
+
+export @npwrite
+macro npwrite(f, x, eltype = :Float32)
+    :(write($f, $(string(x)), convert(Array{$eltype}, permutedims($x)))) |> esc
+end
