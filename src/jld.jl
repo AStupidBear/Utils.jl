9304d360dacc4f91e9993ade62989e9e387940f3
Yao Lu <luyaocns@gmail.com>
2018-05-29 18:49:02 +0800

    jldread jldwrite

---
+++ src/jld.jl
@@ -0,0 +1,22 @@
+export jldwrite
+function jldwrite(filename, name::String, data)
+    fid = jldopen(filename, "r+")
+    try
+        write(fid, name, data)
+    finally
+        close(fid)
+    end
+end
+
+export jldread
+function jldread(filename, name::String)
+    local dat
+    fid = jldopen(filename, "r")
+    try
+        obj = fid[name]
+        dat = read(obj)
+    finally
+        close(fid)
+    end
+    dat
+end
