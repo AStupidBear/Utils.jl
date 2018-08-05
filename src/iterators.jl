fc39bd764f822ccadb42b00bfbfe1fe8d326a5e2
Yao Lu <luyaocns@gmail.com>
2018-08-05 10:48:03 +0800

    firstn lastn

--- src/iterators.jl
+++ src/iterators.jl
@@ -1,2 +1,16 @@
 export droplast
 droplast(x, n = 1) = Iterators.take(x, length(x) - n)
+
+function firstn(itr, n::Integer)
+    a = Vector{eltype(itr)}(n)
+    state = start(itr)
+    for i in 1:n
+        done(itr, state) && return a[1:i - 1]
+        a[i], state = next(itr, state)
+    end
+    return a
+end
+
+firstn(a::AbstractArray, n::Integer) = a[1:min(end, n)]
+
+lastn(a::AbstractArray, n::Integer) = a[max(1, end - n + 1):end]
