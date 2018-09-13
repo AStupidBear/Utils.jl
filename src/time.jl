b36f938f9f25c1a555d587b5f9dc20f30d09c7ef
Yao Lu <luyaocns@gmail.com>
2018-09-13 10:55:55 +0800

    unix2int

--- src/time.jl
+++ src/time.jl
@@ -1,7 +1,8 @@
 using Base.Dates: format, unix2datetime, datetime2unix, Date, Time
 
-export unix2date, unix2time, unix2datetime, datetime2unix
+export unix2date, unix2time, unix2datetime, datetime2unix, unix2intstr, unix2int
 
 unix2date(t) = Date(unix2datetime(t))
 unix2time(t) = Time(unix2datetime(t))
 unix2intstr(t) = format(unix2datetime(t), "yyyymmdd")
+unix2int(t) = parse(Int, unix2intstr(t))
