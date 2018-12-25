c6538a8e50038a28d63e4de8904090cca6e604cb
Yao Lu <luyaocns@gmail.com>
2018-12-25 15:37:45 +0800

    convert to float

--- src/string.jl
+++ src/string.jl
@@ -1,3 +1,3 @@
 export str2float, float2str
 str2float(str) = reinterpret(Float32, resize!(unsafe_wrap(Vector{UInt8}, str), 4))[1]
-float2str(int) = strip(String(reinterpret(UInt8, [int])), '\0')
+float2str(flt) = strip(String(reinterpret(UInt8, Float32[flt])), '\0')
