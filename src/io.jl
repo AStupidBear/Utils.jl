7f6efb3fbf69fd5baf8a96fc7040f381e964572c
Yao Lu <luyaocns@gmail.com>
2019-01-09 13:48:59 +0800

    cycleline

--- src/io.jl
+++ src/io.jl
@@ -6,3 +6,14 @@ readall(f::Union{IO, AbstractString}, T) = reinterpret(T, read(f))
 
 Serialization.serialize(file::AbstractString, x) = open(file, "w") do fid serialize(fid, x) end
 Serialization.deserialize(file::AbstractString) = open(file, "r") do fid deserialize(fid) end
+
+using Base: EachLine
+export cycleline
+function cycleline(filename::AbstractString; chomp=nothing, keep::Bool=false)
+    if chomp !== nothing
+        keep = !chomp
+        depwarn("The `chomp=$chomp` argument to `eachline` is deprecated in favor of `keep=$keep`.", :eachline)
+    end
+    s = open(filename)
+    EachLine(s, ondone=()->seekstart(s), keep=keep)::EachLine
+end
--- src/string.jl
+++ src/string.jl
@@ -1,3 +1,3 @@
 export str2float, float2str
 str2float(str) = reinterpret(Float32, resize!(unsafe_wrap(Vector{UInt8}, str), 4))[1]
-float2str(flt) = strip(String(reinterpret(UInt8, Float32[flt])), '\0')
+float2str(flt) = modf(flt)[1] == 0 ? string(Int(flt)) : strip(String(reinterpret(UInt8, Float32[flt])), '\0')
