be8a66386fd759ca829657f9e294a221bce6baf7
Yao Lu <luyaocns@gmail.com>
2019-02-19 14:06:31 +0800

    unstack bug fix

--- src/array.jl
+++ src/array.jl
@@ -101,13 +101,11 @@ end
 
 export stack, unstack
 stack(xs::Vector{<:AbstractArray}, dim::Integer) = cat(unsqueeze.(xs, dim), dim)
-unstack(xs::Vector{<:AbstractArray}, dim::Integer) = [slicedim(xs, dim, i) for i in 1:size(xs, dim)]
+unstack(xs, dim::Integer) = [copy(selectdim(xs, dim, i)) for i in 1:size(xs, dim)]
 @static if VERSION >= v"0.7"
     stack(xs::AbstractArray...; dims) = stack(collect(xs), dims)
-    unstack(xs::AbstractArray...; dims) = unstack(collect(xs), dims)
 else
     stack(xs::AbstractArray...; dims = 1) = stack(collect(xs), dims)
-    unstack(xs::AbstractArray...; dims = 1) = unstack(collect(xs), dims)
 end
 
 export cstack, rstack
