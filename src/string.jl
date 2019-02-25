6057080d467d967a945269950f4c7dfb79c37f0c
Yao Lu <luyaocns@gmail.com>
2019-02-25 11:45:22 +0800

    str2double

--- src/string.jl
+++ src/string.jl
@@ -1,4 +1,4 @@
-export str2float, float2str
+export str2float, float2str, double2str, str2double
 str2float(str) = reinterpret(Float32, resize!(unsafe_wrap(Vector{UInt8}, str), 4))[1]
 float2str(flt) = modf(flt)[1] == 0 && flt < 7f5 ? string(Int(flt)) : 
                 strip(String(reinterpret(UInt8, Float32[flt])), '\0')
