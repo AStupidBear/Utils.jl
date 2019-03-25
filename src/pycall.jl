c35ee39181dbcc2017149f2fcbf1756fb2dc25ae
Yao Lu <luyaocns@gmail.com>
2019-03-25 14:37:26 +0800

    @imports bug

--- src/pycall.jl
+++ src/pycall.jl
@@ -4,9 +4,21 @@ macro imports(lib, as, abbrev)
     esc(:($abbrev = pyimport($(string(lib)))))
 end
 
-macro imports(libs)
-    libs = isa(libs, Expr) ? libs.args : [libs]
-    exs = [:($lib = pyimport($(string(lib)))) for lib in libs]
+macro imports(lib)
+    exs = []
+    if isa(lib, Expr) && lib.head == :tuple
+        for arg in lib.args
+            var, mdl = arg, string(arg)
+            push!(exs, :($var = pyimport($mdl)))
+        end
+    elseif isa(lib, Expr) && lib.head == :.
+        var, mdl = gensym(lib.args[2].value), string(lib)
+        mdl = replace(mdl, r"[\(\)]" => "")
+        push!(exs, :($var = pyimport($mdl)))
+    else
+        var, mdl = lib, string(lib)
+        push!(exs, :($var = pyimport($mdl)))
+    end
     esc(Expr(:block, exs...))
 end
 
