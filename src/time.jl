2b5939d6dc87119b709526e3f6309169faaf9cc2
Yao Lu <luyaocns@gmail.com>
2019-05-04 17:06:59 +0800

    str2date

--- src/time.jl
+++ src/time.jl
@@ -3,7 +3,8 @@ using Dates: format, unix2datetime, datetime2unix, Date, Time
 export format
 export unix2datetime, datetime2unix
 export unix2date, unix2time
-export unix2str, unix2str8, unix2str6, str2unix
+export unix2str, unix2str8, unix2str6
+export str2date, str2unix
 export unix2int, int2unix
 
 unix2date(t) = Date(unix2datetime(t))
@@ -12,7 +13,8 @@ unix2time(t) = Time(unix2datetime(t))
 unix2str8(t) = format(unix2datetime(t), "yyyymmdd")
 unix2str6(t) = format(unix2datetime(t), "yymmdd")
 const unix2str = unix2str8
-str2unix(str) = datetime2unix(DateTime(str, "yyyymmdd"))
+str2date(str) = DateTime(replace(str, '-' => ""), "yyyymmdd")
+str2unix(str) = datetime2unix(str2date(str))
 
 unix2int(t) = parse(Int, unix2str8(t))
 int2unix(i) = str2unix(string(i))
